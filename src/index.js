36eec59b323740c37e270b0380125aea0c4f536d
mau <mauriciorj76@gmail.com>
2021-05-11 21:59:16 -0600

    primera compilacion

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './componets/App.jsx';
 import './styles/global.scss';
 
+
 ReactDOM.render(<App />, document.getElementById('app'));
---
+++ webpack.config.dev.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+
+    mode: 'develpment',
+
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        })
+    ]
+
+};
