ed82331b317d58105ac9cae25eb007be8a31f6d9
mau <mauriciorj76@gmail.com>
2021-05-11 21:46:47 -0600

    primera compilacion

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
                 ]
             },
             {
-                test: /\.s[ac]ss&/,
+                test: /\.s[ac]ss$/,
                 use: [
                     'style-loader',
                     'css-loader',
